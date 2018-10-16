9dfec1b0555ff16f290905a497731ec9af7a8c29
wikysjw <43368944+wikysjw@users.noreply.github.com>
2018-10-16 09:49:49 +0900

    public 폴더 생성

--- public/sjw_server.js
+++ public/sjw_server.js
@@ -1,10 +1,22 @@
 var http = require('http');
 
+var path = require('path');
+
+var static = require('serve-static');
+
 var socketio = require('socket.io');
+
+var express = require('express');
+
+var app = express();
 
 var cors = require('cors');
 
 app.use(cors());
+
+app.set('port', process.env.PORT || 5034);
+
+app.use('/public', static(path.join(__dirname, 'public')));
 
 var server = http.createServer(app).listen(app.get('port'), function() {
     console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
