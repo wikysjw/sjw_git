71cd302d259243d2a3176e53c0b0721a3da6a0b5
wikysjw <43368944+wikysjw@users.noreply.github.com>
2018-10-17 17:52:35 +0900

    파일 리스트 구현 - 1
    data파일과 csv파일 분리
    리스트를 읽어오나 탭의 내용이 다 출력되는 문제발생

--- sjw_server.js
+++ sjw_server.js
@@ -24,13 +24,6 @@ var server = http.createServer(app).listen(app.get('port'), function() {
     console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
 });
 
-var en = fs.readFile('data/english', 'utf-8', function(error, data) {
-    console.log(data);
-});
-var jp = fs.readFile('data/japanese', 'utf-8', function(error, data) {
-    console.log(data);
-});
-
 var io = socketio.listen(server);
 console.log('응답 대기중...');
 
@@ -40,12 +33,21 @@ io.sockets.on('connection', function(socket) {
     socket.remoteAddress = socket.request.connection._peername.address;
     socket.remotePort = socket.request.connection._peername.port;
 
-    fs.readFile('data/korean', 'utf-8', function(err,data){
+    fs.readdir('data', 'utf-8', function(err, filelist) {
+        socket.emit('filelist',filelist);
+    })
+
+    fs.readFile(`data/english`, 'utf-8', function(err,data){
         socket.emit('language',data);
-        console.log(data);
     });
-    
-    
+
+    fs.readFile(`data/japanese`, 'utf-8', function(err,data){
+        socket.emit('language2',data);
+    });
+
+    fs.readFile(`data/korean`, 'utf-8', function(err,data){
+        socket.emit('language3',data);
+    });
 
     socket.on('message', function(message) {
         console.log('메세지를 받았습니다.');
